03d505fc61f562973dd44fa7d65eb9cfa8c72a58
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-22 18:16:20 -0500

    Code V1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int y;
+int x;
+
+void verifnumero(int x) {
+    if (x == 1) {
+        throw;
+    } else{
+        y=x;
+        cout<<y<<"\n";
+    }
+
+}
+
+void nuevonumero(int y){
+    int final;
+    if (y % 2 == 0){
+        final=y/2;
+        cout<<final<<"\n";
+    }
+    else{
+        final=y*3+1;
+        cout<<final;
+    }
+}
+
+int main() {
+    cin >> x;
+    verifnumero(x);
+    nuevonumero(y);
 }
